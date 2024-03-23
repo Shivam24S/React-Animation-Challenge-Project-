588c158fc6ef54645cd1764889f4647df1cf85b9
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-23 10:05:12 +0530

    re using animate state

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,9 +6,14 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }}
+        // re using animate state
+        variants={{
+          visible: { opacity: 1, y: 0 },
+          hidden: { opacity: 0, y: 30 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >
